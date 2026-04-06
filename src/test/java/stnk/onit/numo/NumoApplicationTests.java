9b1ebbc1382770ac8a628d6992075ce04638652c
Ksenia <ks.borisova2017@gmail.com>
2026-04-06 13:46:28 +0300

    add test

--- src/test/java/stnk/onit/numo/NumoApplicationTests.java
+++ src/test/java/stnk/onit/numo/NumoApplicationTests.java
@@ -1,13 +1,57 @@
 package stnk.onit.numo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import stnk.onit.numo.entity.User;
+import stnk.onit.numo.repository.UserRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
-class NumoApplicationTests {
+@AutoConfigureMockMvc
+class AuthControllerTest {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setEmail("testuser@t");
+        user.setFirstName("Test name");
+        user.setSecondName("Test surname");
+        user.setPassword(passwordEncoder.encode("12345"));
+        userRepository.save(user);
     }
 
+    @Test
+    void loginShouldReturn200() throws Exception {
+        String json = """
+                {
+                  "email": "testuser@t",
+                  "password": "12345"
+                }
+                """;
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+    }
 }
